7c838df8ab45660c524ec6d0e51c753b9dd06e19
Igor Chernyshov <Chernyshov.I.Ser@sberbank.ru>
2021-07-12 08:39:53 +0300

    Initial Commit

---
+++ ChallengeDay50/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChallengeDay50
+//
+//  Created by Igor Chernyshov on 12.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
